f14b311493df8ed303222b0da3f4fdffe80397be
николай нартикоев <nartikoev2015@mail.ru>
2022-01-03 17:44:09 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+def application():
+    app = QApplication(sys.argv)
+
+    windows = QMainWindow()
+    windows.setWindowTitle("Simplex method")
+    windows.setGeometry(0, 0, 300, 250)
+    windows.setBackgroundRole()
+    windows.show()
+
+    sys.exit(app.exec_())
+
+
+application()
