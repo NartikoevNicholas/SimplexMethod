143eeb270d7195183128ed2e8aac8853c9ddd4cc
NartikoevNikolay <nartikoevnikolai@gmail.com>
2022-01-03 22:32:35 +0300

    Change SimplexMethod.py

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -1,6 +1,7 @@
 def getFunctionData(str_function):
-    list_odds = str_function.join(" мир")
+    list_odds = str(str_function).split(";")
+    print(type(list_odds))
     print(list_odds)
 
 
-getFunctionData("привет")
+getFunctionData("3;1;2;4")
