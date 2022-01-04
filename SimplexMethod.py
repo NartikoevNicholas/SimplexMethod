ce35b2b794bdc1561681cec085ce8c42c7b986af
NartikoevNikolay <nartikoevnikolai@gmail.com>
2022-01-05 01:24:16 +0300

    Change SimplexMethod.py

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -26,9 +26,38 @@ def getOddsFunction(str_function, str_extremum, ex=None):
         return ex
 
 
-def getOddsCondition(odds, free_el, ex=None):
+def getOddsCondition(odds, free_el, list_condition, ex=None):
     try:
         result = list()
+        list_odds = list()
+        list_free_el = list()
+        for i in odds:
+            list_temp_odds = list()
+            for j in i.split(";"):
+                list_temp_odds.append(convertStringToNumber(j))
+            list_odds.append(list_temp_odds)
+        for i in free_el:
+            list_temp_free_el = list()
+            for j in i.split(";"):
+                list_temp_free_el.append(convertStringToNumber(j))
+            list_free_el.append(list_temp_free_el)
+        result.append(list_odds)
+        result.append(list_free_el)
+        return result
+    except ex:
+        return ex
+
+
+def getOddsCondition1(odds, free_el, list_condition, ex=None):
+    try:
+        result = list()
+        for index, i in enumerate(list_condition):
+            if i == "<=":
+                pass
+
+
+
+
         list_odds = list()
         list_free_el = list()
         for i in odds:
@@ -59,14 +88,24 @@ def getBasicTableOne(list_function_odds, list_odds_condition, list_condition, ex
                 temp_list.append(temp_odds[_])
             temp_list.append(1 if list_condition[index] == "<=" else -1)
             result.append(temp_list)
+        return result
     except ex:
         return ex
 
 
-func = getOddsFunction("1;2", "max")
+def getSolutionSM(str_function, str_extremum, list_str_odds, list_condition, list_free_element):
+    list_odds_function = getOddsFunction(str_function, str_extremum)
+    if str_extremum == "max":
+        pass
+#       list_odds_condition = getOddsCondition1(list_str_odds,list_free_element,list_condition)
+    else:
+        pass
+    pass
+
+
+func = "-1;2"
+extremum = "max"
 odd = ["-1;1", "1;-2", "1;1"]
 condition = ["<=", "<=", "<="]
 free_e = ["1", "1", "3"]
-cond = getOddsCondition(odd, free_e)
-bt = getBasicTableOne(func, cond, condition)
-print(bt)
+getSolutionSM(func,extremum,odd,condition,free_e)
