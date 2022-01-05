ec6146be345f9f6754fe26f7a3d9b8ceab218ecc
NartikoevNikolay <nartikoevnikolai@gmail.com>
2022-01-05 20:27:15 +0300

    Change SimplexMethod.py

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -1,103 +1,32 @@
-def convertStringToNumber(str_temp, ex=None):
-    try:
-        number = float(str_temp)
-        return number
-    except ex:
-        return ex
-
-
-def getOddsFunction(str_function, str_extremum, ex=None):
-    try:
-        list_odds = str_function.split(";")
-        temp_list = list()
-        if str_extremum == "max":
-            for index, i in enumerate(list_odds):
-                if list_odds[index] == "":
-                    break
-                temp_list.append(convertStringToNumber(list_odds[index]))
-            return temp_list
-        else:
-            for index, i in enumerate(list_odds):
-                if list_odds[index] == "":
-                    break
-                temp_list.append(convertStringToNumber(list_odds[index]) * (-1))
-            return temp_list
-    except ex:
-        return ex
-
-
-def getOddsCondition(odds, free_el, list_condition, ex=None):
+def getListFloatOdds(str_temp, ex=None):
     try:
         result = list()
-        list_odds = list()
-        list_free_el = list()
-        for i in odds:
-            list_temp_odds = list()
-            for j in i.split(";"):
-                list_temp_odds.append(convertStringToNumber(j))
-            list_odds.append(list_temp_odds)
-        for i in free_el:
-            list_temp_free_el = list()
-            for j in i.split(";"):
-                list_temp_free_el.append(convertStringToNumber(j))
-            list_free_el.append(list_temp_free_el)
-        result.append(list_odds)
-        result.append(list_free_el)
+        for i in str_temp.split(";"):
+            if i == "":
+                continue
+            result.append(float(i))
         return result
     except ex:
         return ex
 
 
-def getOddsCondition1(odds, free_el, list_condition, ex=None):
-    try:
-        result = list()
-        for index, i in enumerate(list_condition):
-            if i == "<=":
-                pass
+def getRow(list_odd_condition, str_condition, float_free_element):
+    gf = {'fdf': 3}
+    return gf
 
 
-
-
-        list_odds = list()
-        list_free_el = list()
-        for i in odds:
-            list_temp_odds = list()
-            for j in i.split(";"):
-                list_temp_odds.append(convertStringToNumber(j))
-            list_odds.append(list_temp_odds)
-        for i in free_el:
-            list_temp_free_el = list()
-            for j in i.split(";"):
-                list_temp_free_el.append(convertStringToNumber(j))
-            list_free_el.append(list_temp_free_el)
-        result.append(list_odds)
-        result.append(list_free_el)
-        return result
-    except ex:
-        return ex
-
-
-def getBasicTableOne(list_function_odds, list_odds_condition, list_condition, ex=None):
-    try:    
-        result = list()
-        odds_condition = list_odds_condition[0]
-        for index, i in enumerate(odds_condition):
-            temp_list = list()
-            temp_odds = odds_condition[index]
-            for _, j in enumerate(temp_odds):
-                temp_list.append(temp_odds[_])
-            temp_list.append(1 if list_condition[index] == "<=" else -1)
-            result.append(temp_list)
-        return result
-    except ex:
-        return ex
-
-
-def getSolutionSM(str_function, str_extremum, list_str_odds, list_condition, list_free_element):
-    list_odds_function = getOddsFunction(str_function, str_extremum)
+def getSolutionSM(str_function, str_extremum, list_str_odds, list_condition, list_str_free_element):
+    list_odds_function = getListFloatOdds(str_function)
+    list_free_element = list()
+    list_odds_condition = list()
+    for _, i in enumerate(list_str_odds):
+        list_odds_condition.append(getListFloatOdds(i))
+        list_free_element.append(list_str_free_element[_])
     if str_extremum == "max":
-        pass
-#       list_odds_condition = getOddsCondition1(list_str_odds,list_free_element,list_condition)
+        dict_condition = dict()
+        for _, i in enumerate(list_condition):
+            dict_condition[_] = getRow(list_odds_condition[_], i, list_free_element[_])
+        print(dict_condition)
     else:
         pass
     pass
@@ -108,4 +37,4 @@ extremum = "max"
 odd = ["-1;1", "1;-2", "1;1"]
 condition = ["<=", "<=", "<="]
 free_e = ["1", "1", "3"]
-getSolutionSM(func,extremum,odd,condition,free_e)
+getSolutionSM(func, extremum, odd, condition, free_e)
