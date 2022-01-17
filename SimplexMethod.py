21a9cd6efa357a88b73323b544d9d74df061b4fb
NartikoevNikolay <nartikoevnikolai@gmail.com>
2022-01-17 15:15:02 +0300

    Change SimplexMethod.py

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -20,21 +20,7 @@ def getRow(list_odd_condition, str_condition, float_free_element):
     return result
 
 
-def getSolutionSM(str_function, str_extremum, list_str_odds, list_condition, list_str_free_element):
-    list_odds_function = getListFloatOdds(str_function)
-    list_free_element = list()
-    list_odds_condition = list()
-    for _, i in enumerate(list_str_odds):
-        list_odds_condition.append(getListFloatOdds(i))
-        list_free_element.append(list_str_free_element[_])
-    if str_extremum == "max":
-        dict_condition = dict()
-        for _, i in enumerate(list_condition):
-            dict_condition[_] = getRow(list_odds_condition[_], i, list_free_element[_])
-        print(dict_condition)
-    else:
-        pass
-    pass
+
 
 
 func = "-1;2"
